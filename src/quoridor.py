reward calculation bug fix

src/quoridor.py
@@ -54,8 +54,8 @@ class QuoridorGame:
             self.remove_edge(from_nodes[0], to_nodes[1])
             self.dominoes[player] -= 1
 
-    def get_game_state(self):
-        return np.concatenate((np.copy(self.positions), np.copy(self.dominoes), np.copy(self.board).reshape(self.cols * self.rows)))
+    def get_game_state(self, player):
+        return np.concatenate((np.full(1, player), np.copy(self.positions), np.copy(self.dominoes), np.copy(self.board).reshape(self.cols * self.rows)))
 
     def find_nodes(self, location):
         if location < (self.rows - 1) * (self.cols - 1):

tests/quoridor_test.py
@@ -6,10 +6,10 @@ def test_should_return_init_game_state():
     positions = np.array([0, 1])
     dominoes = np.array([5, 5])
     board = np.full(4, 7)
-    expected = np.concatenate((positions, dominoes, board))
+    expected = np.concatenate((np.zeros(1), positions, dominoes, board))
 
     game = quoridor.QuoridorGame(2, 2, 2)
-    assert np.array_equal(game.get_game_state(), expected)
+    assert np.array_equal(game.get_game_state(0), expected)
 
 
 def test_should_return_init_from_game_state():
@@ -17,11 +17,12 @@ def test_should_return_init_from_game_state():
     dominoes = np.array([0, 0])
     board = np.full(4, 1)
     game_state = np.concatenate((positions, dominoes, board))
+    expected = np.concatenate((np.zeros(1), game_state))
 
     game = quoridor.QuoridorGame(2, 2, 2)
-    assert not np.array_equal(game.get_game_state(), game_state)
+    assert not np.array_equal(game.get_game_state(0), game_state)
     game.init_from_state(game_state)
-    assert np.array_equal(game.get_game_state(), game_state)
+    assert np.array_equal(game.get_game_state(0), expected)
 
 
 def test_should_calculate_correct_number_of_moves():
@@ -37,54 +38,54 @@ def test_should_not_change_player_position_if_no_connection():
     game = quoridor.QuoridorGame(2, 3, 3)
     game.init_from_state(game_state)
     game.do_move(1, 0)
-    assert np.array_equal(game.get_game_state(), game_state)
+    assert np.array_equal(game.get_game_state(0), np.concatenate((np.zeros(1), game_state)))
 
 
 def test_should_not_change_player_position_if_wrong_direction():
     positions = np.array([3, 5])
     dominos = np.array([5, 5])
     board = np.full(9, 7)
-    expected = np.concatenate((positions, dominos, board))
+    expected = np.concatenate((np.zeros(1), positions, dominos, board))
     game = quoridor.QuoridorGame(2, 3, 3)
     game.do_move(3, 0)
-    assert np.array_equal(game.get_game_state(), expected)
+    assert np.array_equal(game.get_game_state(0), expected)
 
 
 def test_should_change_player_position_if_it_is_possible():
     positions = np.array([6, 5])
     dominos = np.array([5, 5])
     board = np.full(9, 7)
-    expected = np.concatenate((positions, dominos, board))
+    expected = np.concatenate((np.zeros(1), positions, dominos, board))
     game = quoridor.QuoridorGame(2, 3, 3)
     game.do_move(2, 0)
-    assert np.array_equal(game.get_game_state(), expected)
+    assert np.array_equal(game.get_game_state(0), expected)
 
 
 def test_should_put_border():
     positions = np.array([0, 1])
     expected_board = np.array([2, 6, 7, 7])
     expected_dominos = np.array([4, 5])
-    expected = np.concatenate((positions, expected_dominos, expected_board))
+    expected = np.concatenate((np.zeros(1), positions, expected_dominos, expected_board))
     game = quoridor.QuoridorGame(2, 2, 2)
     game.do_move(4, 0)
-    assert np.array_equal(game.get_game_state(), expected)
+    assert np.array_equal(game.get_game_state(0), expected)
 
 
 def test_should_not_put_border_on_the_same_place():
     game = quoridor.QuoridorGame(2, 2, 2)
     game.do_move(4, 0)
-    res1 = game.get_game_state()
+    res1 = game.get_game_state(np.zeros(1))
     game.do_move(4, 0)
-    res2 = game.get_game_state()
+    res2 = game.get_game_state(np.zeros(1))
     assert np.array_equal(res1, res2)
 
 
 def test_should_not_put_border_on_the_next_place():
     game = quoridor.QuoridorGame(2, 3, 3)
     game.do_move(4, 0)
-    res1 = game.get_game_state()
+    res1 = game.get_game_state(np.zeros(1))
     game.do_move(5, 0)
-    res2 = game.get_game_state()
+    res2 = game.get_game_state(np.zeros(1))
     assert np.array_equal(res1, res2)
 
 
@@ -94,12 +95,12 @@ def test_should_put_border_on_the_next_available():
     expected_board = np.full(16, 7)
     expected_board[0] = expected_board[2] = 2
     expected_board[1] = expected_board[3] = 6
-    expected = np.concatenate((positions, expected_dominos, expected_board))
+    expected = np.concatenate((np.zeros(1), positions, expected_dominos, expected_board))
     game = quoridor.QuoridorGame(2, 4, 4)
     game.do_move(4, 0)
-    res1 = game.get_game_state()
+    res1 = game.get_game_state(0)
     game.do_move(6, 0)
-    res2 = game.get_game_state()
+    res2 = game.get_game_state(0)
     assert not np.array_equal(res1, res2)
     assert np.array_equal(expected, res2)
 
@@ -107,9 +108,9 @@ def test_should_put_border_on_the_next_available():
 def test_should_not_put_if_vertical_blocked():
     game = quoridor.QuoridorGame(2, 2, 2)
     game.do_move(4, 0)
-    res1 = game.get_game_state()
+    res1 = game.get_game_state(0)
     game.do_move(5, 0)
-    res2 = game.get_game_state()
+    res2 = game.get_game_state(0)
     assert np.array_equal(res1, res2)
 
 